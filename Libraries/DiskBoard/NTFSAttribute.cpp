44a4493f8cbd478db55c70eb9a0a417147c340ed
Adnan Shaheen <shaheen4qau@gmail.com>
2015-12-29 19:35:43 -0500

    NTFS Attribute initial implementation class

---
+++ Libraries/DiskBoard/NTFSAttribute.cpp
@@ -0,0 +1,21 @@
+/**
+* Project : recovery
+*
+* Author : Adnan Shaheen
+*
+*
+* Purpose : Reading NT file system attributes.
+*/
+
+#include "Support.h"
+#include "AbstractLog.h"
+#include "AbstractDisk.h"
+#include "NTFSAttribute.h"
+
+CNTFSAttribute::CNTFSAttribute()
+{
+}
+
+CNTFSAttribute::~CNTFSAttribute()
+{
+}
---
+++ Libraries/DiskBoard/NTFSAttribute.h
@@ -0,0 +1,18 @@
+/**
+* Project : recovery
+*
+* Author : Adnan Shaheen
+*
+*
+* Purpose : reading NT file system attributes.
+*/
+
+#pragma once
+
+class CNTFSAttribute
+{
+public:
+	CNTFSAttribute();
+	virtual ~CNTFSAttribute();
+};
+
