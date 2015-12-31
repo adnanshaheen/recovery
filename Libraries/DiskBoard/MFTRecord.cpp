8629c5923e9dc2d0c7e6a4ae4e8e70454c5029a6
Adnan Shaheen <shaheen4qau@gmail.com>
2015-12-31 02:11:59 -0500

    add remaining items in MFT record

--- Libraries/DiskBoard/MFTRecord.cpp
+++ Libraries/DiskBoard/MFTRecord.cpp
@@ -16,3 +16,28 @@ CMFTRecord::CMFTRecord()
 CMFTRecord::~CMFTRecord()
 {
 }
+
+BOOL CMFTRecord::IsRecordInUse() const
+{
+	return m_uFlags == const_cpu_to_le16(0x0001);
+}
+
+BOOL CMFTRecord::IsRecordDirectory() const
+{
+	return m_uFlags == const_cpu_to_le16(0x0002);
+}
+
+BOOL CMFTRecord::IsRecord4() const
+{
+	return m_uFlags == const_cpu_to_le16(0x0004);
+}
+
+BOOL CMFTRecord::IsRecordViewIndex() const
+{
+	return m_uFlags == const_cpu_to_le16(0x0008);
+}
+
+BOOL CMFTRecord::IsRecordSpace() const
+{
+	return m_uFlags == const_cpu_to_le16(0xFFFF);
+}
--- Libraries/DiskBoard/MFTRecord.h
+++ Libraries/DiskBoard/MFTRecord.h
@@ -17,7 +17,24 @@ public:
 	CMFTRecord();
 	virtual ~CMFTRecord();
 
+	BOOL IsRecordInUse() const;
+	BOOL IsRecordDirectory() const;
+	BOOL IsRecord4() const;
+	BOOL IsRecordViewIndex() const;
+	BOOL IsRecordSpace() const;
+
 private:
 	CNTFSRecord m_nRecord;
+	s64 m_n64LogFileSeq;
+	u16 m_uRecReuse;
+	u16 m_uHardLinks;
+	u16 m_uAttrOffset;
+	u16 m_uFlags;
+	u32 m_uBytesUsed;
+	u32 m_uBytesAllocated;
+	u64 m_u64BaseReference;
+	u16 m_uNextAttr;
+	u32 m_uReserved;
+	u32 m_uRecordNumber;
 };
 
